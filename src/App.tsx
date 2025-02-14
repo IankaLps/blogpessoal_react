6c27ce0fad56b621cf38615677976f189d26031e
IankaLps <iankalp50@gmail.com>
2025-02-14 16:28:28 -0300

    :sparkles: feat: Crie Componente Home e Componentes Navbar e Footer.

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login'; // Certifique-se de importar o componente Login
+
+function App() {
+    return (
+        <>
+            <BrowserRouter>
+                <Navbar />
+                <div className='min-h-[80vh]'>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/login" element={<Login />} />
+                    </Routes>
+                </div>
+                <Footer />
+            </BrowserRouter>
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,27 @@
+import { FacebookLogo, GithubLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+
+function Footer() {
+
+    let data = new Date().getFullYear()
+
+    return (
+        <>
+            <div className="flex justify-center bg-indigo-900 text-white w-full fixed bottom-0 left-0">
+                <div className="container flex flex-col items-center py-4">
+                    <p className='text-xl font-bold'>
+                            Blog Pessoal Generation | Copyright: {data}
+                        </p>
+                    <p className='text-lg'>Acesse nossas redes sociais</p>
+                    <div className='flex gap-2'>
+                        <a href=''/>
+                        <LinkedinLogo size={48} weight='bold' />
+                        <GithubLogo size={48} weight="bold" />
+                        <InstagramLogo size={48} weight='bold' />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Footer
