db1098bd50a698831c82a634d911396649f8b6d3
IankaLps <iankalp50@gmail.com>
2025-02-14 17:01:21 -0300

    :sparkles: feat: O sistema de rotas.

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -13,10 +13,19 @@ function Footer() {
                         </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
-                        <a href=''/>
-                        <LinkedinLogo size={48} weight='bold' />
+
+                    <a href="https://www.linkedin.com/in/iankalps/" target="_blank">
+    	                <LinkedinLogo size={48} weight='bold' />
+                    </a>
+
+                    <a href="https://github.com/IankaLps" target="_blank">
                         <GithubLogo size={48} weight="bold" />
+                    </a>
+
+                    <a href="https://www.linkedin.com/in/seu_usuario" target="_blank">
                         <InstagramLogo size={48} weight='bold' />
+                    </a>
+                    
                     </div>
                 </div>
             </div>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -7,9 +7,7 @@ function Navbar() {
             			   bg-indigo-900 text-white'>
             
                 <div className="container flex justify-between text-lg">
-                    <Link 
-                        to="/home"
-                        className="text-2x1 font-bold">
+                    <Link to="/home" className="text-2x1 font-bold">
                     Blog Pessoal
                     </Link>
                     <div className='flex gap-4'>
